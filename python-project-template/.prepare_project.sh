af4ea9bb764890bcbb3a66fb04fe530312e55dcb
Carl Christofferson <cchris28@uw.edu>
2023-06-09 12:07:59 -0700

    support for older git versions at the cost of making the script less readable

--- python-project-template/.prepare_project.sh
+++ python-project-template/.prepare_project.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
 echo "Initializing local git repository"
-git init --initial-branch=main >/dev/null
+{
+    gitversion=( $(git version | sed 's/^.* //;s/\./ /g') )
+    if let "${gitversion[0]}<2"; then
+	# manipulate directly
+	git init . && echo 'ref: refs/heads/main' >.git/HEAD
+    elif let "${gitversion[0]}==2 & ${gitversion[1]}<40"; then
+	# rename master to main
+	git init . && { git branch -m master main 2>/dev/null || true; };
+    else
+	# set the initial branch name to main
+	git init --initial-branch=main >/dev/null
+    fi
+} > /dev/null
 
 echo "Installing package and runtime dependencies in local environment"
 pip install -e . > /dev/null
