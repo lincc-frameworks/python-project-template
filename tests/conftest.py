e038ed5f979f99bd5d48db4b471ca6910d4ae5f4
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2025-10-09 09:26:25 -0400

    Give up on black on 3.13.

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,5 +14,4 @@ def python_version(request):
 
 @pytest.fixture
 def default_answers(python_version):
-    highest_version_index = PYTHON_VERSIONS.index(python_version)
-    return {"python_versions": PYTHON_VERSIONS[0 : (highest_version_index + 1)]}
+    return {"python_versions": [python_version]}
